1feb85742a2f545331ba86a180670b7526680137
Ammaradonis <156576521+Ammaradonis@users.noreply.github.com>
2025-08-23 17:44:44 +0200

    Delete app/api/schedule/startTime.ts

--- app/api/schedule/startTime.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server"
-import { PrismaClient } from "@prisma/client"
-const prisma = new PrismaClient()
-
-export async function GET(){
-  const sessions = await prisma.classSession.findMany({ take: 10, orderBy: { startTime: 'asc' } })
-  return NextResponse.json(sessions)
-}
